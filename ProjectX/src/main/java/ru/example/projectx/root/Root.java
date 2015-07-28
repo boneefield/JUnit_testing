5d59eab54db8f5ed96ddfedc5bc66607292a9d78
boneefield <starshovatv@gmail.com>
2015-07-28 20:09:10 +0300

    Update Root.java

--- ProjectX/src/main/java/ru/example/projectx/root/Root.java
+++ ProjectX/src/main/java/ru/example/projectx/root/Root.java
@@ -12,6 +12,11 @@ public class Root {
         return roots();
     }
 
+    //поиск одного корня пи D = 0
+    public double[] get_one_X() {
+        return root();
+    }
+
     public Root setA(double a) {
         this.a = a;
         return this;
@@ -25,16 +30,20 @@ public class Root {
         return this;
     }
 
-    private double discriminant() {
+    public double discriminant() {
         return b*b - 4 * a * c;
     }
 
     private double[] roots() {
         System.out.println("Ваше уравнение:\n" + a + "x^2 + " + b + "x + " + c);
-        System.out.println(discriminant());
+        System.out.println("D = " + discriminant());
         double d = discriminant();
         double x1 = (-b + Math.sqrt(d))/2*a;
         double x2 = (-b - Math.sqrt(d))/2*a;
         return new double[]{x1,x2};
+    }
+    private double[] root() {
+        double x = ((-b)/2*a);
+        return new double[]{x};
     }
 }
